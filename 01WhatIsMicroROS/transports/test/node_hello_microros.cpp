c9c52ff0312a0b685932dc9b3efb1d7bbab8f56e
gaolengyuzhou <gaolengyuzhou@outlook.com>
2025-01-03 14:51:58 +0800

    与ESP32的通信示例

---
+++ 01WhatIsMicroROS/transports/test/node_hello_microros.cpp
@@ -0,0 +1,31 @@
+#include <Arduino.h>
+#include <micro_ros_platformio.h>
+
+#include <rcl/rcl.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+
+rclc_support_t support;
+rcl_allocator_t allocator;
+rcl_node_t node;
+
+int counter = 0;
+
+void setup()
+{
+  Serial.begin(115200);
+  // 设置通过串口进行MicroROS通信
+  set_microros_serial_transports(Serial);
+  // 延时时一段时间，等待设置完成
+  delay(2000);
+  // 初始化内存分配器
+  allocator = rcl_get_default_allocator();
+  // 创建初始化选项
+  rclc_support_init(&support, 0, NULL, &allocator);
+  // 创建节点 hello_microros
+  rclc_node_init_default(&node, "hello_microros", "", &support);
+}
+
+void loop()
+{
+}
